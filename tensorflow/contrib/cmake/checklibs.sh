a3c8fdf6d955c74b0abb493ec2ebfcd9ba76482d
Wei Liu <leohoo@hotmail.com>
2018-04-03 11:55:26 +0900

    use regex in find

--- tensorflow/contrib/cmake/checklibs.sh
+++ tensorflow/contrib/cmake/checklibs.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-libs="zlibstatic*.lib libprotobuf*.lib giflib.lib libpng12_static*.lib libjpeg.lib lmdb.lib jsoncpp.lib farmhash.lib fft2d.lib highwayhash.lib nsync.lib sqlite.lib snappy.lib re2.lib tf_protos_cc.lib tf_cc_while_loop.lib tf_cc.lib tf_cc_ops.lib tf_cc_framework.lib tf_core_cpu.lib tf_core_direct_session.lib tf_core_framework.lib tf_core_kernels.lib tf_core_lib.lib"
-for lib in $libs; do 
-  LIB=$(echo "$lib" | sed -e 's/[\r\n]//g'); 
+BUILD_TYPE=Release
+if [ ! -z "$1" ]; then BUILD_TYPE=$1; fi
+
+libs="zlibstatic.?.lib libprotobuf.?.lib giflib.lib libpng12_static.?.lib libjpeg.lib lmdb.lib jsoncpp.lib farmhash.lib fft2d.lib highwayhash.lib nsync.lib sqlite.lib snappy.lib re2.lib tf_protos_cc.lib tf_cc_while_loop.lib tf_cc.lib tf_cc_ops.lib tf_cc_framework.lib tf_core_cpu.lib tf_core_direct_session.lib tf_core_framework.lib tf_core_kernels.lib tf_core_lib.lib"
+for lib in $libs; do
+  LIB=$(echo "$lib" | sed -e 's/[\r\n]//g');
   printf "%30s => " "$LIB"
-  FOUND=$(find . -name "$LIB")
-  if [ -z "$FOUND" ]; then 
+  FOUND=$(find . -regex ".*/$BUILD_TYPE/$LIB")
+  if [ -z "$FOUND" ]; then
     echo -e "\033[35mNOT found.\033[0m";
   else
     echo $FOUND
-  fi; 
+  fi;
 done
